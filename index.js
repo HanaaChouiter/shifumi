c0683c2547f2d89cb1940f1a253e0e611b0e24e0
HanaaChouiter <hanaa7chouiter@gmail.com>
2021-10-04 17:27:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function onButtonClickPierre(){
+    var pierre = document.getElementById("choix")
+    pierre.setAttribute("src", "https://www.jerome-reaux-creations.fr/DVP/codepen/shifumi/1-pierre.jpg")
+}
+
+function onButtonClickFeuille(){
+    var feuille = document.getElementById("choix")
+    feuille.setAttribute("src", "https://www.jerome-reaux-creations.fr/DVP/codepen/shifumi/2-feuille.jpg")
+}
+
+function onButtonClickCiseaux(){
+    var ciseaux = document.getElementById("choix")
+    ciseaux.setAttribute("src", "https://jerome-reaux-creations.fr/DVP/codepen/shifumi/3-ciseaux.jpg")
+}
+
+function onButtonClick()
